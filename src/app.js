3ac269d13caf120881a9cd90232a8452708e9308
Rodrigo Bastos <rodrigobastos@MacBook-Pro-de-Rodrigo.local>
2020-06-17 21:50:14 -0300

    Primeiro Desafio GoStack

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { uuid } = require("uuidv4");
 
 // const { uuid } = require("uuidv4");
 
@@ -11,23 +12,87 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  // const { title, url, techs } = request.query;
+
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs, likes } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title: 'Desafio Node.js',
+    url: 'https://github.com/hellokeeper77/desafio-conceitos-node',
+    techs: [
+      'NodeJs',
+      'JavaScript',
+      'ReactJS'
+    ],
+    likes: 0
+  }
+
+  repositories.push(repository)
+
+  return response.json(repository)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs, likes } = request.body;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found!' })
+  }
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes
+  }
+
+  repositories[repositoryIndex] = repository
+
+  return response.json(repository)
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found!' })
+  }
+
+  repositories.splice(repositoryIndex, 1)
+
+  return response.status(204).send()
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found!' })
+  }
+
+  const oldRepository = repositories[repoIndex]
+
+  const repository = {
+    ...oldRepository,
+    likes:oldRepository.likes + 1
+  }
+
+  repositories[repositoryIndex] = repository
+
+  return response.json(repository)
 });
 
 module.exports = app;
